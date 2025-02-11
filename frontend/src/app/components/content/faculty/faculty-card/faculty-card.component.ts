b863a2f05e34d2935b26f119f8f3fe627bc269a3
Peter Cerven <peter.cerven33@gmail.com>
2025-02-11 11:23:10 +0100

    feat: adding components

---
+++ frontend/src/app/components/content/faculty/faculty-card/faculty-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faculty-card',
+  imports: [],
+  templateUrl: './faculty-card.component.html',
+  styleUrl: './faculty-card.component.scss'
+})
+export class FacultyCardComponent {
+
+}
---
+++ frontend/src/app/components/content/university/university-card/university-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-university-card',
+  imports: [],
+  templateUrl: './university-card.component.html',
+  styleUrl: './university-card.component.scss'
+})
+export class UniversityCardComponent {
+
+}
