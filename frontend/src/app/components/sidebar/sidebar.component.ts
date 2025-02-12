92d869f80e9e0ca9eddbd892d019cfcfbda02902
Peter Cerven <peter.cerven33@gmail.com>
2025-02-12 20:10:48 +0100

    feat: sidebar

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {Component} from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {faChevronLeft, faChevronRight} from '@fortawesome/free-solid-svg-icons';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NgOptimizedImage, FaIconComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-  isSidebarOpen = false;
+  isSidebarOpen = true;
+  faChevronLeft = faChevronLeft;
+  faChevronRight = faChevronRight;
 
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
