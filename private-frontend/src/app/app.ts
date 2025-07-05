345fbb21c20fd496a654c73b5f79904f04c8e6aa
Peter Cerven <peter.cerven33@gmail.com>
2025-07-05 23:13:08 +0200

    feat[private-frontend]: side bar opening and closing

--- private-frontend/src/app/app.ts
+++ private-frontend/src/app/app.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, effect } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { CustomSidenav } from './components/custom-sidenav/custom-sidenav';
+
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, MatToolbar, MatIcon, MatIconModule, MatButtonModule,
-    MatSidenavModule],
+    MatSidenavModule, CustomSidenav],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected title = 'private-frontend';
+
+  constructor() {
+    // effect(() => {
+    //   console.log(this.collapsed());
+    //   console.log(`SideNav width: ${this.sideNavWidth()}`);
+    // });
+  }
+
+
+  collapsed = signal(false);
+
+  sideNavWidth = computed(() => this.collapsed() ? '60px' : '250px');
 }
--- private-frontend/src/app/components/custom-sidenav/custom-sidenav.ts
+++ private-frontend/src/app/components/custom-sidenav/custom-sidenav.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, computed, Input, signal } from '@angular/core';
+import { NgIf, NgOptimizedImage } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+
+export type MenuItem = {
+  icon: string;
+  label: string;
+  route?: string;
+}
 
 @Component({
-  selector: 'app-custom-sidenav',
-  imports: [],
+  selector: 'custom-sidenav',
+  imports: [
+    NgOptimizedImage, MatListModule, MatIconModule, NgIf,
+  ],
   templateUrl: './custom-sidenav.html',
   styleUrl: './custom-sidenav.scss'
 })
 export class CustomSidenav {
 
+  sideNavCollapsed = signal(false);
+  @Input() set collapsed(value: boolean) {
+    this.sideNavCollapsed.set(value);
+  }
+
+  menuItems = signal<MenuItem[]>([
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: 'dashboard'
+    },
+    {
+      icon: 'settings',
+      label: 'Settings',
+      route: 'settings'
+    },
+    {
+      icon: 'help',
+      label: 'Help',
+      route: 'help'
+    },
+    {
+      icon: 'info',
+      label: 'About',
+      route: 'about'
+    }
+    ]);
+
+  profilePicSize = computed(() => this.sideNavCollapsed() ? '32' : '100');
 }
