9daa63093b8752005e8fb4d46eefe38f43ec8592
Peter Cerven <peter.cerven33@gmail.com>
2025-10-09 22:19:31 +0200

    fix[public-frontend]: add transloco config for testing purpose

--- public-frontend/src/app/app.spec.ts
+++ public-frontend/src/app/app.spec.ts
@@ -1,10 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { provideTransloco } from '@jsverse/transloco';
+import { TranslocoHttpLoader } from '@services/transloco-loader.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideTransloco({
+          config: {
+            availableLangs: ['en', 'sk'],
+            defaultLang: 'sk',
+            reRenderOnLangChange: true,
+            prodMode: true,
+          },
+          loader: TranslocoHttpLoader
+        })
+      ]
     }).compileComponents();
   });
 
