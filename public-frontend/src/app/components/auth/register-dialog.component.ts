0ec77c7185b57c26fc5a1aa95347b6404d65d1b4
Andrej Király <kiralyandrej24@gmail.com>
2025-10-22 21:43:12 +0200

    fix[frontend-public]: correct password regex in registration form validator

--- public-frontend/src/app/components/auth/register-dialog.component.ts
+++ public-frontend/src/app/components/auth/register-dialog.component.ts
@@ -138,7 +138,7 @@ export class RegisterDialogComponent implements OnInit {
       password: ['', [
         Validators.required,
         Validators.minLength(8),
-        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/)
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)
       ]],
       confirmPassword: ['', [Validators.required]]
     }, {
