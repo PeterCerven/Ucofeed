b65211c0bcafc4b9fa1d1ca4c2488b66ec96c0b2
Artur_Andriichenko <xandriichenkoa@stuba.sk>
2025-12-12 19:04:22 +0100

    fix(backend): Increase string length in DashboardMessage model. Issue: #22

--- backend/src/main/java/sk/ucofeed/backend/persistence/model/DashboardMessage.java
+++ backend/src/main/java/sk/ucofeed/backend/persistence/model/DashboardMessage.java
@@ -15,7 +15,7 @@ public class DashboardMessage {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 2000)
     private String message;
 
     @CreationTimestamp
